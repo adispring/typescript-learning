8b564749263058024aa3050da7c1eb69f7b8b337
adispring <sunnyadi@163.com>
2021-05-17 23:44:28 +0800

    feat: OmitThisParameter 没有搞懂

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
 const hi: string = 'Hello, Wrold!';
+
+function toHex(this: Number) {
+  return this.toString(16);
+}
+
+type A = typeof toHex;
+
+const fiveToHex: OmitThisParameter<typeof toHex> = toHex.bind(5);
+
+console.log(fiveToHex());
+
+/**
+ * Extracts the type of the 'this' parameter of a function type, or 'unknown' if the function type has no 'this' parameter.
+ */
+type MyThisParameterType<T> = T extends (this: infer U, ...args: any[]) => any ? U : unknown;
+
+/**
+ * Removes the 'this' parameter from a function type.
+ */
+type MyOmitThisParameter<T> = unknown extends ThisParameterType<T>
+  ? T
+  : T extends (this: any, ...args: infer A) => infer R
+  ? (...args: A) => R
+  : T;
+
+type MyThis = MyThisParameterType<typeof toHex>;
+
+type MyOmitThis = MyOmitThisParameter<typeof toHex>;
