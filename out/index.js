d429de188981e14d613e81780767f18fbeb9409a
adispring <sunnyadi@163.com>
2021-05-20 07:08:43 +0800

    feat: vscode 使用 ts-node 直接调试 ts

---
+++ out/index.js
@@ -0,0 +1,3 @@
+var R = require('ramda');
+console.log(R.and({ and: function () { return true; } }, { and: function () { return true; } }));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-const hi: string = 'Hello, Wrold!';
+const R = require('ramda');
 
-function toHex(this: Number) {
-  return this.toString(16);
-}
+console.log(R.and(1, 2));
 
-type A = typeof toHex;
+const a: string = 'h';
 
-const fiveToHex: OmitThisParameter<typeof toHex> = toHex.bind(5);
-
-console.log(fiveToHex());
-
-/**
- * Extracts the type of the 'this' parameter of a function type, or 'unknown' if the function type has no 'this' parameter.
- */
-type MyThisParameterType<T> = T extends (this: infer U, ...args: any[]) => any ? U : unknown;
-
-/**
- * Removes the 'this' parameter from a function type.
- */
-type MyOmitThisParameter<T> = unknown extends ThisParameterType<T>
-  ? T
-  : T extends (this: any, ...args: infer A) => infer R
-  ? (...args: A) => R
-  : T;
-
-type MyThis = MyThisParameterType<typeof toHex>;
-
-type MyOmitThis = MyOmitThisParameter<typeof toHex>;
+console.log(a);
