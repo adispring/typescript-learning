4cafebaa9d030f325862ec517f14cd09e6637977
adispring <sunnyadi@163.com>
2021-12-25 20:47:18 +0800

    feat: noImplicitAny

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import { identity } from './generic';
 
-const a = 'h';
+// const a = 'h';
 // identity(a);
 
 function fn(n: number) {
@@ -18,3 +18,31 @@ function verifyAge(age: number) {
     verified: true;
   }
 }
+
+interface UserDefaults {
+  colorThemeOverride?: 'dark' | 'light';
+}
+declare function getUserSettings(): UserDefaults;
+// ---cut---
+const settings = getUserSettings();
+settings.colorThemeOverride = 'dark';
+settings.colorThemeOverride = 'light';
+
+// But not:
+settings.colorThemeOverride = undefined;
+
+const a: number = 6;
+
+switch (a) {
+  case 0:
+  // console.log('even');
+  case 1:
+    console.log('odd');
+    break;
+}
+
+function fn(s) {
+  // No error?
+  console.log(s.subtr(3));
+}
+fn(42);
