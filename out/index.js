5674b3e03ee40a4cfc975a568c5bdb60c5cf12b2
adispring <sunnyadi@163.com>
2021-12-25 15:42:20 +0800

    feat: 添加 tsc

--- out/index.js
+++
@@ -1,3 +0,0 @@
-var R = require('ramda');
-console.log(R.and({ and: function () { return true; } }, { and: function () { return true; } }));
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-const R = require('ramda');
-
-console.log(R.and(1, 2));
-
-const a: string = 'h';
-
+const a = 'h';
 console.log(a);
