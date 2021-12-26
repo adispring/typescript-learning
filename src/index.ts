8fd3abdf0287cc865b06b780bcc4a0dba7b1ef97
adispring <sunnyadi@163.com>
2021-12-26 18:14:25 +0800

    feat: 存档

--- src/index.ts
+++ src/index.ts
@@ -46,3 +46,46 @@ function fn(s) {
   console.log(s.subtr(3));
 }
 fn(42);
+
+class Album {
+  download() {
+    // Default behavior
+  }
+}
+
+class SharedAlbum extends Album {
+  override download() {
+    // Override to get info from many sources
+  }
+}
+
+class Rectangle {
+  width: number;
+  height: number;
+ 
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+ 
+  getAreaFunction() {
+    return function () {
+      return this.width * this.height;
+    };
+  }
+}
+
+const createKeyboard = (modelID: number) => {
+  const defaultModelID = 23;
+  return { type: "keyboard", modelID };
+};
+
+try {
+  // ...
+} catch (err) {
+  // We have to verify err is an
+  // error before using it as one.
+  // if (err instanceof Error) {
+    console.log(err.message);
+  // }
+}
